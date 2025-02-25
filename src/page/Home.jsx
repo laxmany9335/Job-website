375ae76ca224ca538b4d6d1e6a38af6f765415ba
Laxman Yadav <laxmany9335@.com>
2025-02-25 18:05:51 +0530

    Initial commit

---
+++ src/page/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+           <div className='bg-blue-500 p-5 text-white'>
+               <h1 className='text-3xl'>Home</h1>
+               <p>Welcome to our website</p>        
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
