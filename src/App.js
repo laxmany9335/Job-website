fd3886555d59ad3ea74cec0c4f8d6f0e91282882
Laxman Yadav <laxmany9335@.com>
2025-02-25 18:05:51 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-
-import { Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Login from './page/Login';
-import Signup from './page/Signup';
-import Dashboard from './page/Dashboard';
-import Home from './page/Home';
 
 function App() {
   return (
-    <div className="w-[100vw] h-[100vh] flex justify-center items-center">
-     <Routes>
-       <Route path="/" element={<Home />} />
-       <Route path="/login" element={<Login />} /> 
-       <Route path="/signup" element={<Signup />} />
-       <Route path="/dashboard" element={<Dashboard/>} /> 
-     </Routes>
-      
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
