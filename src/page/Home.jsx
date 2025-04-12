5e1e75b7e4132d2f6c0390838ba19ae344cdb5e9
Laxman Yadav <laxmany9335@.com>
2025-04-12 19:38:52 +0530

    navbar

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../component/common/Navbar'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='w-full'>
+      <Navbar/>
+    </div>
   )
 }
 
